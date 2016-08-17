45bf2b855bbe9f724156dee28043aaf423cf2c43
Denis <feuway@gmail.com>
2016-08-17 07:09:08 +0300

    Доделана валидация

--- build/js/main.js
+++ build/js/main.js
@@ -85,6 +85,52 @@
 	  var formContainer = document.querySelector('.overlay-container');
 	  var formCloseButton = document.querySelector('.review-form-close');
 	
+	  var formReview = document.querySelector('.review-form');
+	  var stars = formReview.elements['review-mark'];
+	  var reviewName = document.querySelector('#review-name');
+	  var reviewText = document.querySelector('#review-text');
+	  var reviewFields = document.querySelector('.review-fields');
+	  var reviewFieldsName = reviewFields.querySelector('.review-fields-name');
+	  var reviewFieldsText = reviewFields.querySelector('.review-fields-text');
+	  var submitButton = document.querySelector('.review-submit');
+	
+	  submitButton.disabled = true;
+	  reviewFieldsText.classList.add('invisible');
+	
+	  reviewName.oninput = validation;
+	  reviewText.oninput = validation;
+	  stars.onchange = validation;
+	
+	  function validation() {
+	    var isNameValid = reviewName.validity.valueMissing;
+	    var isTextValid = reviewText.validity.valueMissing || !reviewText.require;
+	    var isFormValid = isNameValid && isTextValid;
+	
+	    reviewName.required = true;
+	    reviewText.required = stars.value < 3;
+	
+	    submitButton.disabled = isFormValid;
+	
+	    if (!isNameValid) {
+	      reviewFieldsName.classList.add('invisible');
+	    } else {
+	      reviewFieldsName.classList.remove('invisible');
+	    }
+	
+	    if (!reviewText.validity.valueMissing || !reviewText.required) {
+	      reviewFieldsText.classList.add('invisible');
+	    } else {
+	      reviewFieldsText.classList.remove('invisible');
+	    }
+	
+	    if (reviewFieldsName.classList.contains('invisible') &&
+	      reviewFieldsText.classList.contains('invisible')) {
+	      reviewFields.classList.add('invisible');
+	    } else {
+	      reviewFields.classList.remove('invisible');
+	    }
+	  }
+	
 	  var form = {
 	    onClose: null,
 	
@@ -112,72 +158,6 @@
 	
 	  return form;
 	})();
-	
-	var reviewFormField = document.querySelectorAll('.review-form-field');
-	var reviewName = document.querySelector('#review-name');
-	var reviewText = document.querySelector('#review-text');
-	var reviewFields = document.querySelector('.review-fields');
-	var reviewFieldsName = reviewFields.querySelector('.review-fields-name');
-	var reviewFieldsText = reviewFields.querySelector('.review-fields-text');
-	var reviewFormGroupMark = document.querySelector('.review-form-group-mark');
-	var ratingStar = reviewFormGroupMark.querySelectorAll('input');
-	
-	reviewName.required = true;
-	reviewName.oninput = function() {
-	  if (!reviewName.valueMissing) {
-	    reviewFieldsName.style.display = 'none';
-	  } else {
-	    reviewFieldsName.style.display = '';
-	  }
-	};
-	
-	reviewText.oninput = function() {
-	  if (!reviewText.valueMissing) {
-	    reviewFieldsText.style.display = 'none';
-	  } else {
-	    reviewFieldsText.style.display = '';
-	  }
-	};
-	
-	for (var i = 0; i < ratingStar.length; i++) {
-	  if (i > 2) {
-	    ratingStar[i].onchange = function() {
-	      reviewText.required = true;
-	      reviewFieldsText.style.display = '';
-	    };
-	  } else {
-	    ratingStar[i].onchange = function() {
-	      reviewText.required = false;
-	      reviewFieldsText.style.display = 'none';
-	    };
-	  }
-	}
-	
-	reviewFields.onchange = function() {
-	  if ((reviewFieldsName.style.display = 'none') && (reviewFieldsText.style.display = 'none')) {
-	    reviewFields.style.display = 'none';
-	  } else {
-	    reviewFields.style.display = '';
-	  }
-	};
-	
-	// if (!reviewFormField.required.valueMissing) {
-	//     reviewFields.style.display = 'none';
-	// } else {
-	//     reviewFields.style.display = '';
-	// }
-	
-	// if ((reviewName.value.length > 0) && (reviewText.value.length > 0)) {
-	//   reviewFields.style.display = 'none';
-	// }
-	
-	// ratingStar.onchange = function() {
-	//   if ((reviewName.value.length > 0) && (reviewText.value.length > 0)) {
-	//     reviewFields.style.display = 'none';
-	//   } else {
-	//     reviewFields.style.display = '';
-	//   }
-	// };
 
 
 /***/ },
--- src/form.js
+++ src/form.js
@@ -4,6 +4,52 @@ window.form = (function() {
   var formContainer = document.querySelector('.overlay-container');
   var formCloseButton = document.querySelector('.review-form-close');
 
+  var formReview = document.querySelector('.review-form');
+  var stars = formReview.elements['review-mark'];
+  var reviewName = document.querySelector('#review-name');
+  var reviewText = document.querySelector('#review-text');
+  var reviewFields = document.querySelector('.review-fields');
+  var reviewFieldsName = reviewFields.querySelector('.review-fields-name');
+  var reviewFieldsText = reviewFields.querySelector('.review-fields-text');
+  var submitButton = document.querySelector('.review-submit');
+
+  submitButton.disabled = true;
+  reviewFieldsText.classList.add('invisible');
+
+  reviewName.oninput = validation;
+  reviewText.oninput = validation;
+  stars.onchange = validation;
+
+  function validation() {
+    var isNameValid = reviewName.validity.valueMissing;
+    var isTextValid = reviewText.validity.valueMissing || !reviewText.require;
+    var isFormValid = isNameValid && isTextValid;
+
+    reviewName.required = true;
+    reviewText.required = stars.value < 3;
+
+    submitButton.disabled = isFormValid;
+
+    if (!isNameValid) {
+      reviewFieldsName.classList.add('invisible');
+    } else {
+      reviewFieldsName.classList.remove('invisible');
+    }
+
+    if (!reviewText.validity.valueMissing || !reviewText.required) {
+      reviewFieldsText.classList.add('invisible');
+    } else {
+      reviewFieldsText.classList.remove('invisible');
+    }
+
+    if (reviewFieldsName.classList.contains('invisible') &&
+      reviewFieldsText.classList.contains('invisible')) {
+      reviewFields.classList.add('invisible');
+    } else {
+      reviewFields.classList.remove('invisible');
+    }
+  }
+
   var form = {
     onClose: null,
 
@@ -31,42 +77,3 @@ window.form = (function() {
 
   return form;
 })();
-
-var reviewName = document.querySelector('#review-name');
-var reviewText = document.querySelector('#review-text');
-var reviewFields = document.querySelector('.review-fields');
-var reviewFieldsName = reviewFields.querySelector('.review-fields-name');
-var reviewFieldsText = reviewFields.querySelector('.review-fields-text');
-var reviewFormGroupMark = document.querySelector('.review-form-group-mark');
-var ratingStar = reviewFormGroupMark.querySelectorAll('input');
-
-reviewName.required = true;
-reviewName.oninput = function() {
-  if (!reviewName.valueMissing) {
-    reviewFieldsName.style.display = 'none';
-  } else {
-    reviewFieldsName.style.display = '';
-  }
-};
-
-reviewText.oninput = function() {
-  if (!reviewText.valueMissing) {
-    reviewFieldsText.style.display = 'none';
-  } else {
-    reviewFieldsText.style.display = '';
-  }
-};
-
-for (var i = 0; i < ratingStar.length; i++) {
-  if (i > 2) {
-    ratingStar[i].onchange = function() {
-      reviewText.required = true;
-      reviewFieldsText.style.display = '';
-    };
-  } else {
-    ratingStar[i].onchange = function() {
-      reviewText.required = false;
-      reviewFieldsText.style.display = 'none';
-    };
-  }
-}
