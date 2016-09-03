4424d096a54124e5c4a8c98b5555d169c1077ced
Denis <feuway@gmail.com>
2016-09-03 17:08:06 +0300

    Модули

--- src/reviews.js
+++ src/reviews.js
@@ -3,10 +3,9 @@
 module.exports = (function() {
   var loadReviews = require('./load');
   var getReviews = require('./review');
-
   var container = document.querySelector('.reviews-list');
   var URL = 'http://localhost:1506/api/reviews';
-  
+
   function JSONPCallback(reviews) {
     reviews.forEach(function(review) {
       container.appendChild(getReviews(review));
